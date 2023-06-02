d123bc7159a7d5092822ed9a7de11daa7baa06c0
null <72131839+pseudoglock@users.noreply.github.com>
2023-06-02 11:33:55 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let title = 'project';
+let screens = ["Простые, сложные, Интерактивные"];
+let screensPrice = 10;
+let rollback = 50;
+let fullPrice = 100000;
+let adaptive = true;
+
+
+alert("Пососи");
+console.log(title + fullPrice + adaptive);
+console.log(screens.length);
+console.log(screens)
+console.log(fullPrice * (rollback / 100))
